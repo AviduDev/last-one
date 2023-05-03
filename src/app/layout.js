a566261bd07dcd28923f40e64f1f3adf3e2d39cc
AviduDev <avidudev@gmail.com>
2023-05-03 08:02:42 +0530

    Vercel Analytics added

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
 import "./globals.css";
 
+import { Analytics } from "@vercel/analytics/react";
+
 import Nav from "@/components/Nav";
 import Footer from "@/components/Footer";
 
@@ -7,11 +9,18 @@ export const metadata = {
   title: "Web Designer and Fron-tend Developer from Sri Lanka",
   description:
     "Avidu is a creative website designer and front-end developer from sri lanka. Who works with medium size businesses to individuals to increase their online appearance and get more sales.",
-  keywords: ["Web Design", "Web Development", "Frontend", "NextJs", "Avidu", "Design"],
-  creator: 'Avidu Sampath',
-  publisher: 'Avidu Sampath',
-  siteName: 'Avidu',
-  type: 'website',
+  keywords: [
+    "Web Design",
+    "Web Development",
+    "Frontend",
+    "NextJs",
+    "Avidu",
+    "Design",
+  ],
+  creator: "Avidu Sampath",
+  publisher: "Avidu Sampath",
+  siteName: "Avidu",
+  type: "website",
 };
 
 export default function RootLayout({ children }) {
@@ -20,6 +29,7 @@ export default function RootLayout({ children }) {
       <body>
         <Nav />
         {children}
+        <Analytics />
         {/* <Footer /> */}
       </body>
     </html>
