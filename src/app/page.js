44ef8c58f70995a11df553a5c51a4648b3337ee5
AviduDev <avidudev@gmail.com>
2023-05-08 14:12:59 +0530

    Homepage meta tags are added

--- src/app/page.js
+++ src/app/page.js
@@ -2,11 +2,29 @@ import styles from "./page.module.css";
 import Hero from "@/components/Hero";
 import Script from "next/script";
 
-<Script src="//code.jquery.com/jquery.min.js"></Script>
 
 
 {/* <Script src="//code.jquery.com/jquery.min.js"></Script>
 <Script src="dist/descrambler.min.js"></Script> */}
+
+
+export const metadata = {
+  title: "Avidu | Front-End Developer and Designer",
+  description:
+    "Avidu is a creative website designer and front-end developer from sri lanka. Who works with medium size businesses to individuals to increase their online appearance and get more sales.",
+  keywords: [
+    "Web Design",
+    "Web Development",
+    "Frontend",
+    "NextJs",
+    "Avidu",
+    "Design",
+  ],
+  creator: "Avidu Sampath",
+  publisher: "Avidu Sampath",
+  siteName: "Avidu",
+  type: "website",
+};
 
 
 export default function Home() {
